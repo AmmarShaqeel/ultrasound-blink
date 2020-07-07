4f20e2a5d1a8409f0c754298d17b3c438d419b6b
Ammar <17486328+AmmarShaqeel@users.noreply.github.com>
2020-07-07 12:36:30 +0100

    Add files via upload

---
+++ ultrasound_blink.ino
@@ -0,0 +1,101 @@
+
+char trigPin = P1_7;    // Trigger
+char echoPin = P1_4;    // Echo
+char led1 = P2_2;
+char led2 = P2_3;
+char led3 = P1_0;
+char led4 = P2_4;
+char led5 = P2_0;
+long duration, cm;
+long previousMillis, currentMillis;
+const long interval = 5000;  
+int blink = 1;
+
+
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+}
+ 
+void loop() {
+  Serial.println("Running");
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+
+  Serial.println("Running");
+  Serial.print(duration);
+  Serial.print(", ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+
+  if(cm < 250)
+  {
+    currentMillis = previousMillis;
+    previousMillis = millis();
+
+    if (blink == 1) {
+      blink = 0;
+      while (currentMillis - previousMillis <= interval){
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, HIGH);
+      delay(200);
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+      delay(200);
+      currentMillis = millis();
+      }
+    }
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+
+
+  } else {
+    blink = 1;
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+
+    
+  }
+  
+  
+  delay(250);
+}
+
+
